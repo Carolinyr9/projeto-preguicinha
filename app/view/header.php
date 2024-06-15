e99ba8b5002f240513f5ccd6c87ad9b476c1067a
Caroliny Rocha Sampaio <129858370+Carolinyr9@users.noreply.github.com>
2024-06-15 15:25:33 -0300

    💄fix style

--- app/view/header.php
+++ app/view/header.php
@@ -3,8 +3,9 @@
       <a href="#" class="brand-logo">  ☁️ Preguicinha</a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><a href="home.php">Home</a></li>
-        <li><a href="loginFunc.php">Funcionários</a></li>
-        <li><a href="listaprodutos.php">Produtos</a></li>
+        <li><a href="../app/_/loginFunc.php">Funcionários</a></li>
+        <li><a href="../app/_/listaprodutos.php">Produtos</a></li>
+        <li><a href="../app/_/carrinho.php">Carrinho</a></li>
       </ul>
     </div>
   </nav>
