f7d5701bdf00ab226c0c784f276538660e80c4c2
Jessybr <jessicabuenoramos5@gmail.com>
2024-06-19 10:33:47 -0300

    ✨ create client class

---
+++ app/model/cliente.php
@@ -0,0 +1,15 @@
+<?php 
+    require_once '../../config/database.php';
+
+    class Cliente{
+        private $nome;
+        private $email;
+        private $senha;
+        private $endereco;
+        private $cep;
+        private $conn;
+
+
+
+    }
+?>
