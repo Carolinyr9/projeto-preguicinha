68e133e70d437668ce294fe51e283819d0428170
gabrieibarboza <gabrielklarkgk@gmail.com>
2024-06-26 21:53:20 -0300

    🚚 change link

--- app/view/header.php
+++ app/view/header.php
@@ -4,7 +4,7 @@
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><a href="home.php">Home</a></li>
         <li><a href="../app/_/loginFunc.php">Funcionários</a></li>
-        <li><a href="../app/_/listaprodutos.php">Produtos</a></li>
+        <li><a href="../app/produtos.php">Produtos</a></li>
         <li><a href="../app/_/carrinho.php">Carrinho</a></li>
       </ul>
     </div>
