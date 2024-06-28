0068eb3bf36b23d4f938fd6b13b7c857c38ffa03
Jessybr <jessicabuenoramos5@gmail.com>
2024-06-28 17:19:48 -0300

    ✨ add condition to view the header

--- app/view/header.php
+++ app/view/header.php
@@ -1,11 +1,51 @@
-<nav>
-    <div class="nav-wrapper">
-      <a href="#" class="brand-logo">  ☁️ Preguicinha</a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="index.php">Home</a></li>
-        <li><a href="loginFunc.php">Funcionários</a></li>
-        <li><a href="produtos.php">Produtos</a></li>
-        <li><a href="carrinho.php">Carrinho</a></li>
-      </ul>
-    </div>
-  </nav>
+<?php
+if((!isset($_SESSION['funcLogged']) && !isset($_SESSION['clientLogged'])) || 
+    ((isset($_SESSION['funcLogged']) && $_SESSION['funcLogged'] == FALSE) && (isset($_SESSION['clientLogged']) && $_SESSION['clientLogged'] == FALSE))){
+    echo '
+        <nav>
+            <div class="nav-wrapper">
+            <a href="#" class="brand-logo">  ☁️ Preguicinha</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="produtos.php">Produtos</a></li>
+                <li><a href="carrinho.php">Carrinho</a></li>
+                <li><a href="loginCli.php">Clientes</a></li>
+                <li><a href="loginFunc.php">Funcionários</a></li>
+            </ul>
+            </div>
+        </nav>';
+}else{
+    if(isset($_SESSION['funcLogged']) && $_SESSION['funcLogged'] == TRUE){
+        echo '
+            <nav>
+                <div class="nav-wrapper">
+                <a href="#" class="brand-logo">  ☁️ Preguicinha</a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="listarFunc.php">Funcionários</a></li>
+                    <li><a href="listarCli.php">Produtos</a></li>
+                    <li><a href="listarCli.php">Cliente</a></li>
+                    <li><a href="../controller/logoff.php">Sair</a></li>
+                </ul>
+                </div>
+            </nav>';
+    }else{
+        if(isset($_SESSION['clientLogged']) && $_SESSION['clientLogged'] == TRUE){
+            echo '
+                <nav>
+                    <div class="nav-wrapper">
+                    <a href="#" class="brand-logo">  ☁️ Preguicinha</a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="produtos.php">Produtos</a></li>
+                        <li><a href="carrinho.php">Carrinho</a></li>
+                    </ul>
+                    </div>
+                </nav>
+            ';
+        }
+    }
+}
+
+?>
+
