ec4c4c5cd716b051d1b346099ab626317bae9492
Jessybr <jessicabuenoramos5@gmail.com>
2024-06-29 15:04:30 -0300

    ✨ init session

--- app/controller/logoff.php
+++ app/controller/logoff.php
@@ -1,5 +1,7 @@
-<?php 
+<?php
+session_start(); // Inicia a sessão antes de destruí-la
 session_unset();
 session_destroy();
 header('Location: ../view/produtos.php');
+exit; // Adicione exit após o redirecionamento
 ?>
